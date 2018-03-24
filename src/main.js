55a84d21413dc8ebf97a0703fa1475c1633126e0
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-03-23 17:17:48 -0700

    add front end logic to main.js

--- src/main.js
+++ src/main.js
@@ -4,22 +4,22 @@ import $ from 'jquery';
 import 'bootstrap';
 
 $(document).ready(function() {
-  $('#list').click(function() {
-    let museumName = $('#museumName').val();
-    $('#museumName').val("");
+  $('#issueOutput').click(function() {
+    let issue= $('#issue').val();
+    $('#issue').val("");
 
     $.ajax({
       url:
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.6061,-122.3421&radius=500&types=museum&name=${museumName}&key=AIzaSyDSFM9lXQaRZG4pnnUg5CtGyiT_Gjb0lCU`,
-      
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=37.773,-122.413,100&skip=2&limit=10&user_key=2066f67a6ef0762a8ac45f20e50d011f`,
+
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        for(let i = 0; i < response.results.length; i++){
-          $('#name').text(`The name of ${museumName} is ${response.results[i].name}.`);
-          $('#address').text(`The location is ${response.results[i].vicinity}.`);
+        for(let i = 0; i < response.data.profile.length; i++){
+          $('#showList').text`A list of doctors that specialized in this medial issue are following ${response.data.profile.last_name}.`);
+          ;
         }
       },
       error: function() {
