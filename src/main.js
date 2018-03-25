4b4e268b987cd94d25225b09d0768704e96494a4
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-03-24 21:06:01 -0700

    return error if user enters issue name or doctors name that don't exist

--- src/main.js
+++ src/main.js
@@ -16,12 +16,13 @@ $(document).ready(function() {
         format: 'json'
       },
       success: function(response) {
+        if(response.data.length === 0){
+          $('#showList').text(`Sorry, there are no doctors that meet your search criteria`)
+        }else{
+          for(let i = 0; i < response.data.length; i++){
 
-        for(let i = 0; i < response.data.length; i++){
-
-          $('#showList').append(`<p>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>`);
-
-
+            $('#showList').append(`<p>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>`);
+          }
         }
       },
       error: function(request, status, error) {
@@ -31,32 +32,36 @@ $(document).ready(function() {
     });
 
     $('#doctorOutput').click(function() {
-     let doctorName = $('#doctorName').val();
+      let doctorName = $('#doctorName').val();
       $('#doctorName').val("");
 
 
-    $.ajax({
-      url:
-      `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2066f67a6ef0762a8ac45f20e50d011f`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
+      $.ajax({
+        url:
+        `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2066f67a6ef0762a8ac45f20e50d011f`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          if(response.data.length === 0){
+            $('#showDetails').text(`Sorry, there are no doctors that meet your search criteria`)
+          }else{
 
-        for(let i = 0; i < response.data.length; i++){
+          for(let i = 0; i < response.data.length; i++){
 
-          $('#showDetails').append(`<p>Doctor's name:${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>
-        <p>Address:${response.data[i].practices[0].visit_address.street},  ${response.data[i].practices[0].visit_address.city}</p>`,);
+            $('#showDetails').append(`<p>Doctor's name:${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p>
+            <p>Address:${response.data[i].practices[0].visit_address.street},   ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} ${response.data[i].practices[0].visit_address.zip}</p>
+            <p>Phone: ${response.data[i].practices[0].phones[0].number}</p> <p>Website:${response.data[i].practices[0].website}</p> <p>Accepts new patients:${response.data[i].practices[0].accepts_new_patients}</p>`);
 
+}
+          }
+        },
+        error: function(request, status, error) {
 
+          $('#errors').text("There was an error processing your request. Please try again.")
         }
-      },
-      error: function(request, status, error) {
-
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
+      });
     });
   });
-});
 });
